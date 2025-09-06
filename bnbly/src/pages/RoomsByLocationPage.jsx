ffa836b25c112c036518dcb8d2fa0f7049ec5864
Olukoya Esther <estherbose18@gmail.com>
2025-09-06 09:14:29 +0300

    Minor Tweaks

--- bnbly/src/pages/RoomsByLocationPage.jsx
+++ bnbly/src/pages/RoomsByLocationPage.jsx
@@ -6,11 +6,12 @@ const RoomsByLocationPage = () => {
   const rooms = useLoaderData();
 
   if (!rooms || rooms.length === 0) {
-    return <p className="flex items-center justify-center h-screen text-[1.2rem] text-red-600 bg-black">No rooms in this location..</p>
+    return <p className="flex items-center justify-center h-screen text-[1.2rem] text-red-600">No rooms in this location..</p>
   }
 
   return (
     <>
+    <h2 className="mt-4 text-center text-gray-500">Result for Apartments in {rooms[0].location}... </h2>
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 px-8 py-12">
       {rooms.map((room) => (
           <RoomCard
